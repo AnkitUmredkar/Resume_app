a31a31e675a82bf44cbb66e8e555f24be105d722
Ankit <ankitumredkar77@gmail.com>
2024-05-21 18:05:14 +0530

    Initial commit

--- lib/Screens/InformationPage/global.dart
+++ lib/Screens/InformationPage/global.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+import '../../utils/global.dart';
+
+String birthDate = '';
+String gender = 'Male';
+
+TextEditingController birthDateController = TextEditingController();
+
 ListTile options(String data, IconData icon) {
   return ListTile(
     leading: Icon(icon, size: 24),
@@ -20,8 +27,10 @@ OutlineInputBorder buildOutlineInputBorder(double borderWidth) {
   );
 }
 
-TextField buildTextField(int maxLine, String label) {
+TextField buildTextField(int maxLine, String label, var typeOfKeyboard) {
   return TextField(
+    keyboardType: typeOfKeyboard,
+    textInputAction: TextInputAction.next,
     maxLines: maxLine,
     decoration: InputDecoration(
         enabledBorder: buildOutlineInputBorder(1.5),
@@ -39,12 +48,12 @@ Row buildRow(double width, String data) {
         style: TextStyle(
             fontWeight: FontWeight.bold,
             color: Colors.grey,
-            fontSize: width * 0.045),
+            fontSize: width * 0.0405),
       ),
       const Text(
         ' *',
         style: TextStyle(
-            color: Colors.red, fontWeight: FontWeight.bold, fontSize: 17),
+            color: Colors.red, fontWeight: FontWeight.bold, fontSize: 18),
       ),
     ],
   );
@@ -58,7 +67,32 @@ Align text(double width, String data) {
       style: TextStyle(
           fontWeight: FontWeight.bold,
           color: Colors.grey,
-          fontSize: width * 0.045),
+          fontSize: width * 0.0405),
+    ),
+  );
+}
+
+Container buttons(double width, String data) {
+  return Container(
+    alignment: Alignment.center,
+    height: 50,
+    width: width / 2.4,
+    decoration: BoxDecoration(
+        color: blueColor,
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: const [
+          BoxShadow(
+              spreadRadius: 1,
+              blurRadius: 10,
+              offset: Offset(7, 7),
+              color: Colors.grey),
+        ]),
+    child: Text(
+      data,
+      style: TextStyle(
+          color: Colors.white,
+          fontSize: width * 0.045,
+          fontWeight: FontWeight.bold),
     ),
   );
 }
